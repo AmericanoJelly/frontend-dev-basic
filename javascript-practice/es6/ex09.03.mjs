e0648e4d173cca786d56a7ba7819498d8637fa52
YUJIN <yujinj704@gmail.com>
2022-07-12 15:00:31 +0900

    mjs

---
+++ javascript-practice/es6/ex09.03.mjs
@@ -0,0 +1,14 @@
+/**
+ * unamed exportI
+ * 
+ * 1. exports.name = ...: commonJS(require)와 유사하다.
+ * 2. 이 모듈을 import 할 때에는 destructing이 가능하다.
+ */
+
+export const add = function(a, b){
+    return a + b;
+}
+
+export const subtract = function(a, b){
+    return a - b;
+}
